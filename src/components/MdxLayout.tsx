fe0ef6201ae88e39554b5e26acd299d6a2357319
aoirint <aoirint@gmail.com>
2021-11-21 03:20:29 +0900

    syntax highlight

--- src/components/MdxLayout.tsx
+++ src/components/MdxLayout.tsx
@@ -2,6 +2,15 @@ import React from 'react'
 import Helmet from 'react-helmet'
 import '../styles/main.scss'
 
+// import Prism from 'prismjs'
+// import 'prismjs/plugins/custom-class/prism-custom-class'
+// Prism.plugins.customClass.prefix('prism--')
+// Prism.plugins.customClass.map({
+//   number: 'prism--number',
+// })
+
+import Highlight, { defaultProps } from "prism-react-renderer"
+import vsDark from 'prism-react-renderer/themes/vsDark'
 import {
     MDXProvider
 } from '@mdx-js/react'
@@ -12,6 +21,27 @@ import {
 import {
   Navbar,
 } from '../components'
+
+const CodeBlock: React.FC<MDXRendererProps> = (props) => {
+  const codeProps = props.children.props
+  const codeString = codeProps.children.trim()
+  const language = /language-(\w+)/.exec(codeProps.className)[1]
+  return (
+    <Highlight {...defaultProps} code={codeString} language={language} theme={vsDark}>
+      {({ className, style, tokens, getLineProps, getTokenProps }) => (
+        <pre className={className} style={style}>
+          {tokens.map((line, i) => (
+            <div {...getLineProps({ line, key: i })}>
+              {line.map((token, key) => (
+                <span {...getTokenProps({ token, key })} />
+              ))}
+            </div>
+          ))}
+        </pre>
+      )}
+    </Highlight>
+  )
+}
 
 const MdxLayout: React.FC<MDXRendererProps> = (props) => {
     return (
@@ -24,8 +54,9 @@ const MdxLayout: React.FC<MDXRendererProps> = (props) => {
                 <div className='container'>
                   <div className='content'>
                     <MDXProvider
-                        components={{
-                        }}
+                      components={{
+                        pre: CodeBlock,
+                      }}
                     >
                         {props.children}
                     </MDXProvider>
