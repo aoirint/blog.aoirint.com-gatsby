8a7f7c1e32aa0a94268bf76edb466bfb6ce31daf
aoirint <aoirint@gmail.com>
2022-03-29 05:28:20 +0900

    update gtag

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -244,7 +244,7 @@ const config: GatsbyConfig = {
       resolve: "gatsby-plugin-google-gtag",
       options: {
         trackingIds: [
-          "G-STE0958LN0",
+          "G-33TXF7WL2L",
         ],
         pluginConfig: {
           head: true,
